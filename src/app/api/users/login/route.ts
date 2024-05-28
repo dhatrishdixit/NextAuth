2f909f6331618dc07898fb43d7f72028a0b153e5
dhatrishdixit <dhatrish29@gmail.com>
2024-05-28 23:51:18 +0530

    dont understand this fucking cant can OverrwriteModelError

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,51 @@
+import {connectDB} from "@/dbConfig/dbConfig";
+import { User } from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connectDB();
+
+export async function POST(request:NextRequest){
+    try {
+        const reqBody = await request.json();
+        const {email,password}= reqBody ;
+        const user = await User.findOne({email});
+        if(!user) return NextResponse.json({
+            error: "User not found",
+            status:404
+        })
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch) return NextResponse.json({
+            error: "Invalid credentials",
+            status:401
+        })
+
+        const tokenData = {
+            id:user._id
+        };
+        if(!process.env.TOKEN_SECRET) return NextResponse.json({
+            error: "Token secret not found",
+            status:500
+        });
+        const token = jwt.sign(tokenData,process.env.TOKEN_SECRET,{
+            expiresIn: "1d"
+        });
+        const response = NextResponse.json({
+            message: "Login successful",
+            success: true,
+        })
+        response.cookies.set("token", token, {
+            httpOnly: true, 
+            
+        })
+        // return response ;
+    } 
+    catch (error:any) {
+        return NextResponse.json({
+            error: error.message,
+            status:500
+        })
+    }
+}
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,31 +1,52 @@
 import nodemailer from 'nodemailer';
+import bcrypt from "bcryptjs";
+import { User } from "@/models/userModel";
 
-export async function sendEmail(email:string,emailType:string,userId:string){
+interface sendEmailProps{
+  email:string,
+  emailType:string,
+  userId:string
+}
+export async function sendEmail({
+  email,
+  emailType,
+  userId
+}:sendEmailProps){
+    
+    
     try {
+      const hashedToken = await bcrypt.hash(userId.toString(), 10)
+
+      if (emailType === "VERIFY") {
+          await User.findByIdAndUpdate(userId, 
+              {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000})
+      } else if (emailType === "RESET"){
+          await User.findByIdAndUpdate(userId, 
+              {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000})
+      }
 
        
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // Use `true` for port 465, `false` for all other ports
-            auth: {
-              user: "maddison53@ethereal.email",
-              pass: "jn7jnAPss4f63QBp6D",
-            },
-          });
+      var transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "ecd43a06430d6f",
+          pass: "224b0c00f37451"
+        }
+      });
+      const mailOptions = {
+        from: 'dhatrish29@gmail.com',
+        to: email,
+        subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+        html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+        or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+        </p>`
+    }
 
-          const mailOptions = {
-            from:  'dhatrish29@gmail.com', // sender address
-            to: email, 
-            subject: emailType == "VERIFY" ? "verify your email":"reset your password", // Subject line
-         
-            html: "<b>Hello world?</b>", // html body
-          }
+       const mailresponse = await transport.sendMail
+       (mailOptions);
+       return mailresponse;
 
-          const info = await transporter.sendMail(mailOptions);
-        
-          console.log("Message sent: %s", info.messageId);
-          return info ;
 
     } catch (error) {
         console.log("some error in mailer: %s", error)
